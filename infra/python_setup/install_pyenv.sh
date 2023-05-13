963ca0870e9db66aa19cbbfbc0fe2984b4dbc6f9
YeonwooSung <neos960518@gmail.com>
2023-05-13 15:58:21 +0900

    feat: Add scripts for installing pyenv and python3.8

---
+++ infra/python_setup/install_pyenv.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+REPO_ROOT_PATH=$(git rev-parse --show-toplevel)
+
+# 1. install pyenv
+
+PYTHON_VERSION="3.8.6"
+VIRTUAL_ENV="dev"
+PYENV_DIR=~/.pyenv
+
+MYSHELL=`echo "$SHELL" | awk -F/ '{print $NF}'`
+echo "SHELL: $MYSHELL"
+if [ ! -d $PYENV_DIR ]; then
+    git clone https://github.com/yyuu/pyenv.git $PYENV_DIR
+    git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+    TARGET=~/.bashrc
+    if [ "$MYSHELL" = "zsh" ]; then
+        TARGET=~/.zshrc
+    fi
+
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $TARGET
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $TARGET
+    echo 'eval "$(pyenv init --path)"' >> $TARGET
+    echo 'eval "$(pyenv virtualenv-init -)"' >> $TARGET
+
+    echo "source $TARGET"
+fi
