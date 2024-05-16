3044e9f58285cc012ace94c6ca72c2637e25a225
YeonwooSung <neos960518@gmail.com>
2024-05-16 22:59:25 +0900

    feat: Learn fuzzy search in postgres FTS

---
+++ data-centric-apps/sql/postgresql/full_text_search/sql/fts_fuzzy_typo_search.sql
@@ -0,0 +1,14 @@
+
+-- The similarity funciton is based on the pg_trgm extension.
+-- So, we need to create the extension first.
+CREATE EXTENSION pg_trgm;
+
+-- similarity(text, text) → float
+-- The similarity function returns a number that indicates how similar two strings are.
+-- The number is between 0 and 1, where 1 means the strings are identical.
+-- This function is based on the pg_trgm extension, which provides functions and operators for determining the similarity of text values based on trigram matching.
+SELECT to_tsquery('balloon |' ||
+                  string_agg(word, ' | ')
+       )
+FROM data_words
+WHERE similarity(word, 'balloon') > 0.4;
